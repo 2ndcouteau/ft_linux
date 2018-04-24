f5a8d93f84105fd8490ce0e464b691b0d71f0e35
yoko <qrosa@student.42.fr>
2018-04-24 03:53:57 +0000

    [FIX] \!\! CRITIC \!\! change call to rm -rf command. Now $SOURCES must be call for each folder \!\!

--- 5.end/3_remove_all_folder.sh
+++ 5.end/3_remove_all_folder.sh
@@ -1,91 +1,91 @@
 #!/bin/bash
 SOURCES="/mnt/lfs/sources/"
-cd $SOURCES
-# /
+
+#
 echo -e $blu"Remove Bash"$Rcol
-sudo rm -rf bash*/
+sudo rm -rf $SOURCES/bash*/
 #
 echo -e $blu"Remove Bison"$Rcol
-sudo rm -rf bison*/
+sudo rm -rf $SOURCES/bison*/
 #
 echo -e $blu"Remove Bzip2"$Rcol
-sudo rm -rf bzip*/
+sudo rm -rf $SOURCES/bzip*/
 #
 echo -e $blu"Remove Coreutils"$Rcol
-sudo rm -rf coreutil*/
+sudo rm -rf $SOURCES/coreutil*/
 #
 echo -e $blu"Remove DejaGNU"$Rcol
-sudo rm -rf dejagnu*/
+sudo rm -rf $SOURCES/dejagnu*/
 #
 echo -e $blu"Remove Expect"$Rcol
-sudo rm -rf expect*/
+sudo rm -rf $SOURCES/expect*/
 #
 echo -e $blu"Remove M4"$Rcol
-sudo rm -rf m4*/
+sudo rm -rf $SOURCES/m4*/
 #
 echo -e $blu"Remove Ncurses"$Rcol
-sudo rm -rf ncurse*/
+sudo rm -rf $SOURCES/ncurse*/
 #
 echo -e $blu"Remove tcl-core"$Rcol
-sudo rm -rf tcl*/
+sudo rm -rf $SOURCES/tcl*/
 #
 ##
 NAME_P="diffutils"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="file"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="findutils"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="gawk"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="gettext"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="grep"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="gzip"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="make"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="patch"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="perl"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="sed"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
-NAME_P=/
+NAME_P="tar"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="texinfo"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="util-linux"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
 #
 NAME_P="xz"
 echo -e $blu"Remove $NAME_P"$Rcol
-sudo rm -rf $NAME_P*/
+sudo rm -rf $SOURCES/$NAME_P*/
